e93b01dbe33169e3d60ae2df54e80dd8e5075b59
Sergey Kuznetsov <108385338+SD131313@users.noreply.github.com>
2022-08-07 16:38:28 +0300

    Update 03_even.py

--- Module4/practice/funcs_part0/03_even.py
+++ Module4/practice/funcs_part0/03_even.py
@@ -1,9 +1,9 @@
-def even(...):
-    ...
-    return ...
+def even(n):
+    if n % 2 == 0:
+        print(f"Число {n} четное")
+    else:
+        print("Число не четное")
+    return n
 
-n = ...
-if even(n):
-   print("Число четное")
-else:
-   print("Число не четное")
+
+print(even(26))
